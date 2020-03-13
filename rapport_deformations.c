f1892afbba246be485184712fa10498301d427c2
acosC <50544684+acosC@users.noreply.github.com>
2020-03-13 14:43:48 +0100

    Baaaase

---
+++ rapport_deformations.c
@@ -0,0 +1,127 @@
+#include "rapport_deformations.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static clock_t temps_cpu;
+
+void redemarrer_chronometre()
+{
+    temps_cpu = clock();
+}
+
+int relever_chronometre_ms()
+{
+    return (clock() - temps_cpu)/((double) CLOCKS_PER_SEC)*1000;
+}
+
+/* tire aléatoirement une valeur réelle selon une distribution triangulaire
+ * centrée en 1/2 et de largeur 1 */
+double distribution_triangulaire(){
+    int valeur;
+    int accepte = 0;
+    while (!accepte){
+        valeur = rand();
+        if (valeur <= RAND_MAX/2){
+            accepte = (rand() % RAND_MAX/2) <= valeur;
+        }else{
+            accepte = (rand() % RAND_MAX/2) <= RAND_MAX - valeur;
+        }
+    }
+    return valeur/(double) RAND_MAX;
+}
+
+/* tire aléatoirement une position selon une distribution triangulaire */
+Position position_distr_triang(Position centre, int largeur)
+{
+    return centre - largeur/2 + (int) (largeur*distribution_triangulaire());
+}
+
+/* transformation d'un tableau de positions tirées uniformément en une
+ * distribution quasi-monotone */
+/* fonction auxiliaire : ordonne les positions autour d'une valeur pivot */
+int distribuer_pivot(Position* positions, int n)
+{
+    Position pivot = positions[0];
+    int i = 1;
+    int j = n;
+    while (i < j){
+        if (positions[i] <= pivot){
+            i++;
+        }else{
+            j--;
+            Position tmp = positions[j];
+            positions[j] = positions[i];
+            positions[i] = tmp;
+        }
+    }
+    if (i == n){
+        positions[0] = positions[n - 1];
+        positions[n - 1] = pivot;
+        return n - 1;
+    }
+    return i;
+}
+
+/* transformation : tri partiel */
+void transf_quasi_monotone(Position* deformations, int nombre_deformations,
+    int arret)
+{
+    if (nombre_deformations < arret){ return; }
+    int i = distribuer_pivot(deformations, nombre_deformations);
+    transf_quasi_monotone(deformations, i, arret);
+    transf_quasi_monotone(deformations + i, nombre_deformations - i, arret);
+}
+
+Position* simuler_deformations(Position nombre_positions,
+    int nombre_deformations, Simulation simulation)
+{
+    Position* paquet = malloc(sizeof(Position)*nombre_deformations);
+    if (!paquet){
+        fprintf(stderr, "Nombre de déformations trop grand, pas assez de "
+            "mémoire.\n");
+        exit(EXIT_FAILURE);
+    }
+    int i;
+    if (simulation == UNIFORME){
+        /* distribution uniforme */
+        for (i = 0; i < nombre_deformations; i++){
+            paquet[i] = rand() % nombre_positions;
+        }
+    }else{
+        if (simulation == MONOMODALE){
+            /* distribution triangulaire centrée sur la position centrale,
+             * largeur quart */
+            Position centre = nombre_positions/4;
+            int largeur = nombre_positions/4;
+            for (i = 0; i < nombre_deformations; i++){
+                paquet[i] = position_distr_triang(centre, largeur);
+            }
+        }else if (simulation == BIMODALE){
+            /* deux distributions triangulaires centrées sur les positions en 
+             * premier et troisième quartile, largeur huitième */
+            int largeur = nombre_positions/8;
+            for (i = 0; i < nombre_deformations; i++){
+                Position centre = (rand() % 2) ?
+                    nombre_positions/4 : 3*nombre_positions/4;
+                paquet[i] = position_distr_triang(centre, largeur);
+            }
+        }else if (simulation == QUASIMONOTONE){
+            for (i = 0; i < nombre_deformations; i++){
+                paquet[i] = rand() % nombre_positions;
+            }
+            /* une valeur d'arrêt assez grande assure des inversions */
+            int arret = SEUIL_ALERTE;
+            transf_quasi_monotone(paquet, nombre_deformations, arret);
+        }else{
+            fprintf(stderr, "Configuration %i invalide.\n", simulation);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return paquet;
+}
+
+void detruire_deformations(Position* paquet)
+{
+    free(paquet); 
+}
---
+++ rapport_deformations.h
@@ -0,0 +1,39 @@
+#include <time.h> /* pour la mesure du temps de calcul */
+#define PRECISION 1 /* en m */
+#define DIST_MAX_VOISIN 100 /* en m */
+#define SEUIL_ALERTE 100 /* en nombre de déformations */
+
+/***  mesure du temps de calcul  ***/
+void redemarrer_chronometre();
+int relever_chronometre_ms();  /* en millisecondes */
+
+/***  modelisation rudimentaire ***/
+typedef int Position;
+struct Alerte{
+    Position position;
+    int nombre_deformations_locales;
+};
+typedef struct Alerte Alerte;
+
+/***  simulation d'un paquet de deformations  ***/
+
+/* différentes configurations des déformations */
+enum Simulation {UNIFORME, MONOMODALE, BIMODALE, QUASIMONOTONE};
+typedef enum Simulation Simulation; 
+
+/* Simulation proprement dite
+ *
+ *     ENTREES
+ * nombres_positions : le nombre de positions possibles (les positions sont 
+ *     repérées par des entiers entre 0 et nombre_positions - 1)
+ * nombre_deformations : le nombre de déformations simulées
+ * simulation : configuration des déformations considérées
+ *
+ *     SORTIE
+ * pointeur sur un paquet de deformations, c'est-à-dire un tableau de positions
+ * comprises entre 0 et nombres_positions - 1, de taille nombre_deformations */
+Position* simuler_deformations(Position nombre_positions,
+    int nombre_deformations, Simulation simulation);
+
+/* Libère la mémoire utilisée par un paquet de déformations */
+void detruire_deformations(Position* paquet);
