dce673963af350e0cd8f3ef8a9cd9e7e47364ac4
Antoine Creuzet <antoine@controller.access.network>
2020-03-13 17:53:59 +0100

    coucou c nous

--- alerte_deformation.c
+++ alerte_deformation.c
@@ -1,54 +1,83 @@
 #include "rapport_deformations.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define LONGUEUR_CABLE 100000 /* en m */
-#define NOMBRE_DEFORMATIONS 100000 /* en nombre de déformations */
+#define LONGUEUR_CABLE 1000 /* en m */
+#define NOMBRE_DEFORMATIONS 100 /* en nombre de dï¿½formations */
 #define PAS_VOISIN
 
 /**
-        On simule des déformations sur un clable de longueur donnée et de précision donnée
-        On définie le nombre de déformation que l'on veut
-        Les déformation sont répartie, en fonction d'une loi, sur l'ensemble des positions du cable
-        Pour chaque déformation une position sera atribué
-        l'alerte est donné si il y a 100 déformations pour une position et les positions voisine
+        On simule des dï¿½formations sur un clable de longueur donnï¿½e et de prï¿½cision donnï¿½e
+        On dï¿½finie le nombre de dï¿½formation que l'on veut
+        Les dï¿½formation sont rï¿½partie, en fonction d'une loi, sur l'ensemble des positions du cable
+        Pour chaque dï¿½formation une position sera atribuï¿½
+        l'alerte est donnï¿½ si il y a 100 dï¿½formations pour une position et les positions voisine
         Deux positions sont voisine si elle sont distante de 100m ou moins
-        Soit si pos(200) est étudié, toute les position comprise entre pos(100) et pos(300) sont voisine
+        Soit si pos(200) est ï¿½tudiï¿½, toute les position comprise entre pos(100) et pos(300) sont voisine
 **/
 
 int main()
 {
-    /* paramètres */
+    /* paramï¿½tres */
     int nombre_positions = LONGUEUR_CABLE/PRECISION;
     int nombre_deformations = NOMBRE_DEFORMATIONS;
     Simulation simulation = UNIFORME; //type de loi suivie pour la simulation
 
-    int temps_ecoule; /* pour chronométrer */
+    int temps_ecoule; /* pour chronomï¿½trer */
 
-    /** simulation des déformation **/
+
+    /** simulation des dï¿½formation **/
     printf("Simulation de %i deformations sur %i positions, configuration %i... ", nombre_deformations, nombre_positions, simulation);
     redemarrer_chronometre();//pour calculer le temps de la simulation
     Position* paquet = simuler_deformations(nombre_positions,nombre_deformations, simulation);
     printf("terminee.\n");
 
+
+    /** deuxiÃ¨me version **/
+  int i=0;
+  int position,position2,max,min,k,limite_deformation;
+  printf("c'est chiant.\n");
+  for(i=0;i<LONGUEUR_CABLE;i++)
+  {
+    position = paquet[i];
+    if (position>900)
+      max=LONGUEUR_CABLE-1;
+    else
+      max=position+100;
+    if (position<100)
+      min=0;
+    else
+      min=position-100;
+    for(k=0;k<LONGUEUR_CABLE;k++)
+    {
+      position2=paquet[k];
+      if ((min<position2) && (position2<max))
+      {
+        limite_deformation++;
+        if (limite_deformation =100)
+          printf("Alerte : position%i\n",i);
+      }
+    }
+  }
+
     const char* nom_fichier = "simulation_deformations.dat";
     printf("Ecriture du paquet de deformations dans %s... ", nom_fichier);
 
-    FILE* file = fopen(nom_fichier, "w"); // création du fichier qui va contenir les positions des déformations
-    for (int i = 0; i < nombre_deformations; i++) //écriture dans le fichier
+    FILE* file = fopen(nom_fichier, "w"); // crï¿½ation du fichier qui va contenir les positions des dï¿½formations
+    for (int i = 0; i < nombre_deformations; i++) //ï¿½criture dans le fichier
     {
         fprintf(file, "%i ", paquet[i]);
     }
     fclose(file); //fermeture du fichier
     printf("terminee.\n");
 
-    temps_ecoule = relever_chronometre_ms();// on récupère le temps pris pour cette opération
+    temps_ecoule = relever_chronometre_ms();// on rï¿½cupï¿½re le temps pris pour cette opï¿½ration
     printf("Temps ecoule pour la repartition des deformations : %i ms.\n", temps_ecoule);
 
     //detruire_deformations(paquet);
 
     /** premiere version **/
-
-    redemarrer_chronometre(); //démarache du chronomètre pour calculer le temps d'une possible alerte
+/*
+    redemarrer_chronometre(); //dï¿½marache du chronomï¿½tre pour calculer le temps d'une possible alerte
     int tab_position[LONGUEUR_CABLE];
 
     for(long position =0; position<LONGUEUR_CABLE; position++)
@@ -62,8 +91,16 @@ int main()
         printf("position %i nb deformation %i \n",position,tab_position[position]);
     }
 
-    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps écoulé pour cette version
+    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps ï¿½coulï¿½ pour cette version
     printf("Temps ecoule premiere version : %i ms.\n", temps_ecoule);
 
     return 0;
+*/
+
+
+
+
+
+
+
 }
