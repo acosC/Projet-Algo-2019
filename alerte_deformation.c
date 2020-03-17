308b4f9c7e99dd1056385ff9d16977eb2171bdb8
este411 <62145788+este411@users.noreply.github.com>
2020-03-17 16:33:23 +0100

    Add files via upload
    update de l'organisation des fichiers

--- alerte_deformation.c
+++ alerte_deformation.c
@@ -1,69 +1,58 @@
-#include "rapport_deformations.h"
+#include "fonction.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define LONGUEUR_CABLE 100000 /* en m */
-#define NOMBRE_DEFORMATIONS 100000 /* en nombre de déformations */
-#define PAS_VOISIN
+
 
 /**
         On simule des déformations sur un clable de longueur donnée et de précision donnée
         On définie le nombre de déformation que l'on veut
         Les déformation sont répartie, en fonction d'une loi, sur l'ensemble des positions du cable
         Pour chaque déformation une position sera atribué
-        l'alerte est donné si il y a 100 déformations pour une position et les positions voisine
-        Deux positions sont voisine si elle sont distante de 100m ou moins
+        ainsi la fonction simuler_deformations renvoi un tableau de positions pour chaque déformation
+        l'alerte est donné si il y a 100 déformations pour une position et les positions voisine (le seuil peut être modifié)
+        Deux positions sont voisine si elle sont distante de 100m ou moins (cette variable peut être modifié)
         Soit si pos(200) est étudié, toute les position comprise entre pos(100) et pos(300) sont voisine
 **/
 
 int main()
 {
-    /* paramètres */
+    int choix =0;
     int nombre_positions = LONGUEUR_CABLE/PRECISION;
-    int nombre_deformations = NOMBRE_DEFORMATIONS;
-    Simulation simulation = UNIFORME; //type de loi suivie pour la simulation
-
-    int temps_ecoule; /* pour chronométrer */
+    Simulation simulation = BIMODALE; //type de loi suivie pour la simulation
 
     /** simulation des déformation **/
-    printf("Simulation de %i deformations sur %i positions, configuration %i... ", nombre_deformations, nombre_positions, simulation);
-    redemarrer_chronometre();//pour calculer le temps de la simulation
-    Position* paquet = simuler_deformations(nombre_positions,nombre_deformations, simulation);
-    printf("terminee.\n");
+    Position* paquet = simuler_deformations(nombre_positions,NOMBRE_DEFORMATIONS, simulation);
+    rapport(paquet,"simulation_deformations.dat"); //ecriture des positions pour chaque déformation dans un fichier
 
-    const char* nom_fichier = "simulation_deformations.dat";
-    printf("Ecriture du paquet de deformations dans %s... ", nom_fichier);
-
-    FILE* file = fopen(nom_fichier, "w"); // création du fichier qui va contenir les positions des déformations
-    for (int i = 0; i < nombre_deformations; i++) //écriture dans le fichier
+    while(choix != 4)
     {
-        fprintf(file, "%i ", paquet[i]);
-    }
-    fclose(file); //fermeture du fichier
-    printf("terminee.\n");
+        /** menu **/
+        printf("\nquel version souhaitez vous executer?\n");
+        printf("[1] premiere version\n");
+        printf("[2] deuxieme version\n");
+        printf("[3] troisieme version\n");
+        printf("[3] exit\n");
+        scanf(" %i",&choix);
 
-    temps_ecoule = relever_chronometre_ms();// on récupère le temps pris pour cette opération
-    printf("Temps ecoule pour la repartition des deformations : %i ms.\n", temps_ecoule);
-
-    //detruire_deformations(paquet);
-
-    /** premiere version **/
-
-    redemarrer_chronometre(); //démarache du chronomètre pour calculer le temps d'une possible alerte
-    int tab_position[LONGUEUR_CABLE];
-
-    for(long position =0; position<LONGUEUR_CABLE; position++)
-    {
-        tab_position[position] =0;
-        for (long deformation =0; deformation<NOMBRE_DEFORMATIONS; deformation++)
+        switch(choix)
         {
-            if (paquet[deformation] == position)
-                tab_position[position] = tab_position[position] + 1;
+        case 1:
+            Premiere_version(paquet,"rapport_premiere_version.dat");
+            break;
+        case 2:
+            Deuxieme_version(paquet,"rapport_deuxieme_version.dat");
+            break;
+        case 3:
+            Troisieme_version(paquet,"rapport_troisieme_version.dat");
+            break;
+        case 4:
+            break;
+        default :
+            printf("choix invalide");
+            break;
         }
-        printf("position %i nb deformation %i \n",position,tab_position[position]);
     }
 
-    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps écoulé pour cette version
-    printf("Temps ecoule premiere version : %i ms.\n", temps_ecoule);
-
+    free(paquet);
     return 0;
 }
---
+++ fonction.c
@@ -0,0 +1,159 @@
+#include "fonction.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/** fonctions pour meusurer le temps écoulé pour l'execution d'une version **/
+static clock_t temps_cpu;
+void redemarrer_chronometre()
+{
+    temps_cpu = clock();
+}
+int relever_chronometre_ms()
+{
+    return (clock() - temps_cpu)/((double) CLOCKS_PER_SEC)*1000;
+}
+
+/** PREMIERE VERSION **/
+/** valeur limite: longueur 100 000 nb deformation 100 000 **/
+/** temps de création du tableau tab_position beaucoup trop long au dela de la limite **/
+void Premiere_version(Position * paquet,const char* nom_fichier)
+{
+    redemarrer_chronometre(); //démarache du chronomètre pour calculer le temps d'une possible alerte
+    printf("premiere version en cours d'execution...");
+
+    int temps_ecoule;
+    int* tab_position = malloc(LONGUEUR_CABLE * sizeof(int));//utilisation d'une allocation dynamique pour gérer une grande quantité de valeur
+
+    /* création d'un tableau de transfère */
+    /* tab_position est tableau des déformation en fonction des positions */
+    /* pour chaque position on a le nombre de déformation en cette position */
+    /* c'est cette étape qui prend le plus de temps car on parcours 1 000 000 de fois 100 000 déformation */
+
+    for(int position =0; position<LONGUEUR_CABLE; position++)
+    {
+        tab_position[position] =0;
+        for (int deformation =0; deformation<NOMBRE_DEFORMATIONS; deformation++)
+        {
+            if (paquet[deformation] == position)
+                tab_position[position] = tab_position[position] + 1;
+        }
+    }
+
+    /* détection des alertes */
+    /* pour chaque position on calcul un min et un max qui représente les deux voisin extréma */
+    /* création d'un rapport d'alerte */
+    FILE* file = fopen(nom_fichier, "w");
+    int somme,somme_alerte=0, min, max;
+    for(int position =0; position<LONGUEUR_CABLE; position++)//on parcours le tableau créé au dessus
+    {
+        somme =0;
+
+        if (position>LONGUEUR_CABLE - DIST_MAX_VOISIN - 1)
+            max =LONGUEUR_CABLE-1;// car les case vont de 0 à long LONGUEUR_CABLE-1 soit LONGUEUR_CABLE case
+        else
+            max = position + DIST_MAX_VOISIN;
+
+        if (position<DIST_MAX_VOISIN)//pour éviter que le min soit au en dessous de 0 car impossible
+            min =0;
+        else
+            min =position - DIST_MAX_VOISIN;
+
+        for(int i =min; i<=max;i++)//on parcours de tableau du min au max inclu et on fait la somme des déformations
+            somme = somme + tab_position[i];
+
+        /* edition du rapport */
+        if(somme>SEUIL_ALERTE)//si le seuil d'alerte est atteind
+        {
+            somme_alerte++;
+            fprintf(file,"alerte position %i avec %i deformation\n",position,somme);
+        }
+    }
+    fclose(file); //fermeture du fichier
+
+    printf("termine\n");
+    printf("nb d'alerte : %i\n",somme_alerte);
+    free(tab_position);
+
+    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps écoulé pour cette version
+    printf("Temps ecoule premiere version : %i ms.\n", temps_ecoule);
+}
+
+/** DEUXIEME VERSION **/
+void Deuxieme_version(Position * paquet,const char* nom_fichier)
+{
+    redemarrer_chronometre();
+    printf("deuxieme version en cours d'execution...");
+
+    int temps_ecoule;
+
+    /** CODE **/
+
+    printf("termine\n");
+
+    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps écoulé pour cette version
+    printf("Temps ecoule deuxieme version : %i ms.\n", temps_ecoule);
+}
+
+/** TROISIEME VERSION **/
+/** optimisation de la PREMIERE VERSION */
+void Troisieme_version(Position * paquet,const char* nom_fichier)
+{
+    redemarrer_chronometre(); //démarache du chronomètre pour calculer le temps d'une possible alerte
+
+    printf("troisieme version en cours d'execution\n");
+
+    int temps_ecoule;
+    int* tab_position = malloc(LONGUEUR_CABLE * sizeof(int));//utilisation d'une allocation dynamique pour gérer une grande quantité de valeur
+
+    /* création d'un tableau de transfère */
+    /* tab_position est tableau des déformation en fonction des positions */
+    /* pour chaque position on a le nombre de déformation en cette position */
+    /* optimiser par rapport à la PREMIERE VERSION */
+
+    for (int position =0; position<LONGUEUR_CABLE; position++) //on initilaise à 0 par sécurité
+        tab_position[position] =0;
+
+    for (int deformation =0; deformation < NOMBRE_DEFORMATIONS; deformation++)
+    {
+        tab_position[paquet[deformation]]++; //paquet[deformation] est une position donc on parcours le paquet de déformation pour chaque position lu on ajoute +1 à la position corespondante
+    }
+
+    /* détection des alertes */
+    /* pour chaque position on calcul un min et un max qui représente les deux voisin extréma */
+    /* crétion d'un rapport d'alerte */
+    /* identique à la premiere version */
+    FILE* file = fopen(nom_fichier, "w");
+    int somme,somme_alerte=0, min, max;
+    for(int position =0; position<LONGUEUR_CABLE; position++)
+    {
+        somme =0;
+
+        if (position>LONGUEUR_CABLE - DIST_MAX_VOISIN - 1)
+            max =LONGUEUR_CABLE-1;
+        else
+            max = position + DIST_MAX_VOISIN;
+
+        if (position<DIST_MAX_VOISIN)
+            min =0;
+        else
+            min =position - DIST_MAX_VOISIN;
+
+        for(int i =min; i<=max;i++)
+            somme = somme + tab_position[i];
+
+        /* edition du rapport */
+        if(somme>SEUIL_ALERTE)
+        {
+            somme_alerte++;
+            fprintf(file,"alerte position %i avec %i deformation\n",position,somme);
+        }
+    }
+    fclose(file); //fermeture du fichier
+
+    printf("termine\n");
+    printf("nb d'alerte : %i\n",somme_alerte);
+    free(tab_position);
+
+    temps_ecoule = relever_chronometre_ms(); //on recuperrer le temps écoulé pour cette version
+    printf("Temps ecoule troisieme version : %i ms.\n", temps_ecoule);
+}
